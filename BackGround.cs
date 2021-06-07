f650e62b474866a1129e15a3f830ebf9a358a40b
1521097974 <78798852+1521097974@users.noreply.github.com>
2021-06-07 14:33:43 +0800

    Update BackGround.cs

--- BackGround.cs
+++ BackGround.cs
@@ -25,11 +25,13 @@ public class BackGround : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float fParrlaxX = (previousCamPos.x - cam.position.x) * fparallax;//上一帧减去当前帧的位置
-        for (int i=0;i<backgrounds.Length;i++)
+        float fParrlaxX = (previousCamPos.x - cam.position.x) * fparallax;//上一帧减去当前帧的位置，模拟透视的效果
+        for (int i=0;i<backgrounds.Length;i++)//对每一层进行运算
         {
             float fNewX = backgrounds[i].position.x + fParrlaxX * (1 + i * layerFraction);
+
             Vector3 newPos = new Vector3(fNewX, backgrounds[i].position.y, backgrounds[i].position.z);
+
             backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, newPos, fSmooth * Time.deltaTime);
         }
         previousCamPos = cam.position;
